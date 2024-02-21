709d0f654f4a51fead9a58ccb5c179fc42abe2ec
Maratib Ali Khan <maratib@gmail.com>
2024-02-22 03:08:33 +0500

    updated

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-code ~/iwork/learn/hardhat/hardhat-contracts --extensionDevelopmentPath=/Users/maratib/iwork/learn/vscode/samples/vscode-extension-playground/ --disable-extensions --force
+code --disable-extensions --force ~/iwork/learn/hardhat/hardhat-contracts --extensionDevelopmentPath=/Users/maratib/iwork/learn/vscode/samples/vscode-extension-playground/ 
